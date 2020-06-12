74dd3dbd122d56c7008bf3d66281c878e670d2bf
Richard Hayler <topshed@users.noreply.github.com>
2020-06-12 14:14:49 +0100

    Update install_msteams.sh

--- install_msteams.sh
+++ install_msteams.sh
@@ -4,4 +4,5 @@ if [[ $response =~ ^[Yy]$ ]]
 then
     sudo wget https://raw.githubusercontent.com/RaspberryPiFoundation/pidrop-msteams-installer/master/chromium-browser-msteams.desktop -O /usr/share/applications/chromium-browser-msteams.desktop
 fi
+echo "All done!"
 
