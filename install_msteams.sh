6ab5e49da784a52268c0a2b8e763a57f70144ffd
Richard Hayler <topshed@users.noreply.github.com>
2020-06-12 13:59:55 +0100

    Update install_msteams.sh
    added confirmation check

--- install_msteams.sh
+++ install_msteams.sh
@@ -1,1 +1,7 @@
-wget https://raw.githubusercontent.com/RaspberryPiFoundation/pidrop-msteams-installer/master/chromium-browser-msteams.desktop -O /usr/share/applications/chromium-browser-msteams.desktop
+read -p "Install Chromium browser for MS Teams? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo wget https://raw.githubusercontent.com/RaspberryPiFoundation/pidrop-msteams-installer/master/chromium-browser-msteams.desktop -O /usr/share/applications/chromium-browser-msteams.desktop
+fi
+
